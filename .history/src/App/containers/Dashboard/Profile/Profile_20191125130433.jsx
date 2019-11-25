cef5be4eb3b15293cf05fba1b9a4e28bb881b19f
Veronika Manikina <DelMare@mail.ru>
2019-11-25 13:05:37 +0300

    Profile

--- .history/src/App/containers/Dashboard/Profile/Profile_20191125130433.jsx
+++ .history/src/App/containers/Dashboard/Profile/Profile_20191125130433.jsx
@@ -87,4 +87,41 @@ class Profile extends Component {
 }
  
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
-
+// class Profile extends React.Component {
+//   render() { 
+//     return ( <div>
+//         <h1>Профиль</h1>
+//         <p>Способ оплаты</p>
+//         <form>
+//           <div>
+//             <div>
+//                 <label>Номер карты</label>
+//             </div>
+//             <div>
+//                 <input type="text" name="CardNumber" placeholder="0000 0000 0000 0000" />
+//             </div>
+//             <div>
+//                 <input type="text" name="Date" placeholder="11/19" />
+//             </div>
+//           </div>
+//           <div>
+//             <div>
+//                 <label>Имя владельца</label>
+//             </div>
+//             <div>
+//                 <input type="text" name="CardName" placeholder="User Name" />
+//             </div>
+//             <div>
+//               <label>CVC</label>
+//               <input type="text" name="cvc" placeholder="CVC" />
+//             </div>
+//           </div>
+//           <div>
+//             <button>Сохранить</button>
+//           </div>
+//         </form>
+//     </div> 
+//     );
+//   }
+// }
+ 
